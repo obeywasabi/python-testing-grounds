a4a64b670a66f84e0b932c051178096393eda4f8
obeywasabi <116626623+obeywasabi@users.noreply.github.com>
2022-10-29 19:59:14 -0700

    redo menu logic
    - added some checks for menu
    - added more vault code
    - retouched some code

--- mypyPJS/Password Manager/Password Manager (Non-GUI) V0.1.py
+++ mypyPJS/Password Manager/Password Manager (Non-GUI) V0.1.py
@@ -1,19 +1,23 @@
-## Password generator - Non-GUI Based, Alex Arias 10.27.2022
+## Password Generator - Non-GUI Based, Alex Arias 10.27.2022
 import random
+import os
 
-print("Password manager (non-gui) v.0.1 by Alex A")
+print("Password manager (non-gui) v.0.3 by Alex A")
 print()
 input("Press any key to continue")
 
 #def clear():
- #   os.system("cls")
+#   os.system("cls")
 
-mainmenu = True
+run = True
+mainmenu = False
+menu_state = 0
 newpassword = False
-generator = False
 vault = False
 did_reroll = False
-choice = ""
+menu_choice = ""
+
+#USER Variables
 origin = ""
 fav_number = ""
 color = ""
@@ -25,106 +29,121 @@ def regen():
     result = ''.join(str(item) for item in re_roll)
     print("Your new password is" + result,"$")
 
+def getvault():
+    print("Checking if key file exits....")
+    result = os.path.exists('key.txt')
+
+    if result:
+        print("Welcome back!")
+
+    elif result == False:
+        print("No key file found!")
+
+
 # MAIN MENU OPTIONS
+while run:
+    mainmenu = True
+    while mainmenu and menu_state == 0:
+        print("What would you like to do?")
+        print()
+        print("(1) Create New Password")
+        print("(2) Store Password to Vault (Coming Soon)")
+        print("(3) Access Password Vault (Coming Soon)")
+        print("(4) Quit")
+        print()
+        menu_choice = int(input("> "))
 
-while mainmenu:
-    print("What would you like to do?")
-    print()
-    print("(1) Create New Password")
-    print("(2) Store Password to Vault (Coming Soon)")
-    print("(3) Access Password Vault (Coming Soon)")
-    print("(4) Quit")
-    try:
-        choice = int(input("> "))
-    except ValueError:
-        print("Use a number!!")
-        input("Press enter to continue")
+        if menu_choice == 1:
+            newpassword = True
+            mainmenu = False
+            menu_state = menu_state + 1
 
-#MENU CHOICE 1
+        elif menu_choice == 2:
+            vault = True
+            menu_state = menu_state + 5
+            mainmenu = False
 
-    if choice == 1:
-        newpassword = True
-        mainmenu = False
+        elif menu_choice == 4:
+            quit()
 
-# NEW PASSWORD CONDITION
-        while newpassword:
-            print("Okay, you want to generate a new password, fill in these quick questions to generate")
-            origin = input("What is your background? ")
-            fav_number = input("Type a favorite number ")
-            color = input("Type your favorite color(s) ")
-            phrase = input("Type a phrase you'd like to be included in your password ")
-            print("Generating....")
-            print()
-            regen()
-            print()
-            try:
-                choice = int(input("Type 1 to return to main menu , or, type 2 and press enter to re-roll "))
-            except ValueError:
-                print("Invalid Choice!")
-                input("Press enter to return to main menu")
+# NEW PASSWORD STATE
+    while newpassword and menu_state == 1:
+        menu_choice = ""
+        print("Okay, you want to generate a new password, fill in these quick questions to generate")
+        origin = input("What is your background? ")
+        fav_number = input("Type a favorite number ")
+        color = input("Type your favorite color(s) ")
+        phrase = input("Type a phrase you'd like to be included in your password ")
+        print("Generating....")
+        print()
+        regen()
+        print()
 
-            if choice == 1:
-                newpassword = False
-                mainmenu = True
+        menu_choice = int(input("Type 1 to return to main menu , or, type 2 and press enter to re-roll "))
 
-# IF RE-ROLLING
-            elif choice == 2:
-                did_reroll = True
-                generator = False
-                newpassword = False
+        if menu_choice == 1:
+            menu_state = menu_state - 1
+            mainmenu = True
+            newpassword = False
 
-            else:
-                print("Invalid Option!")
-                generator = False
-                newpassword = False
-                did_reroll = True
-                choice = int(input("Type 1 to return to main menu , or, type 2 and press enter to re-roll "))
+        if menu_choice == 2:
+            newpassword = False
+            did_reroll = True
+            menu_state = menu_state + 1
+
+        elif menu_choice > 2:
+            print("Invalid Option!")
+            newpassword = False
+            did_reroll = True
+            input("Press Enter to continue")
 
 # WHILE RE-ROLLING
-        while did_reroll:
-            print("Re-rolling....")
+    while did_reroll and menu_state == 2:
+        print("Re-rolling....")
+        regen()
+        choice_sub = ""
+        choice_sub = int(input("Type 1 to re-roll or type 2 to return to main menu "))
+
+        if choice_sub == 1:
+            newpassword = False
             regen()
-            choice_sub = ""
-            choice_sub = int(input("Type 1 to re-roll or type 2 to return to main menu "))
 
-            if choice_sub == 1:
-                newpassword = False
-                regen()
+        elif choice_sub == 2:
+            mainmenu = True
+            menu_state = menu_state - 2
+            did_reroll = False
 
-            elif choice_sub == 2:
-                did_reroll = False
-                generator = False
-                mainmenu = True
+        else:
+            #choice_sub != 1 or not(2 != choice_sub)
+            print("Invalid option!")
+            input("Press enter to re-roll")
+            #did_reroll = True
 
-            else:
-                #choice_sub != 1 or not(2 != choice_sub)
-                print("Invalid option!")
-                input("Press enter to re-roll")
-                #did_reroll = True
+
+## VAULT VARIABLES
 
 did_consent = False
 is_IDValid = False
 has_ID = False
 
-def getvault():
-    os.path.exists('vaultkey.txt')
-    if os.path.exists:
-        print("Welcome back!")
-    elif
-    print("No vault key found! Create a new one?")
+## VAULT FUNCTIONS
 
-vault_choice = ""
-input("Type Yes or "Y" to create a new key, or press type No or "N" to return to main menu:> ")
+## VAULT FUNCTION CODE WAS HERE
+container = open("key.txt,", "w")
 
-def answer
+while vault and menu_state == 5:
+    getvault()
+    vault_choice = input("Type Yes or 'Y' to create a new key, or press type No or 'N' to return to main menu:> ")
 
-#MENU CHOICE 2
+    if vault_choice == "Y":
+        key = random.seed(10)
+        open("key.txt", "w")
+        container.write(str(key))
+        container.close()
+        print("Key successfully generated!")
 
-    if choice == 2:
-        vault = True
-        getvault()
+    elif vault_choice == "N":
+        vault = False
+        mainmenu = True
 
 
-
-    if choice == 4:
-        quit()
